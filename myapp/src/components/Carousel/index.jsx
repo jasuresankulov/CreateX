4a82f69a6b922c8107387fb1722a8c5019b5dc3e
esankulov <jasuresankulov07@gmail.com>
2024-09-29 01:10:05 +0500

    fix some details in carousel

--- myapp/src/components/Carousel/index.jsx
+++ myapp/src/components/Carousel/index.jsx
@@ -6,44 +6,74 @@ import FirstBigImg from "../../assets/images/firstBgImg.jpg"
 
 function CarouselComponet() {
   return (
-    <>
-     <div className="main">
+
+    <div className="main">
       <Carousel
         showThumbs={false}
         autoPlay={true}
         infiniteLoop={true}
         showStatus={false}
       >
-       
+
         <div className="car">
-          <h1>CREATEX</h1>
-          <h1>CONSTRUCTION</h1>
+          <div className="text">
+            <h1>CREATEX</h1>
+            <h1>CONSTRUCTION</h1>
+            <p>Cras ultrices leo vitae non viverra. Fringilla nisi quisque consequat, dignissim vitae proin ipsum sed. Pellentesque nec turpis purus eget pellentesque integer ipsum elementum felis. </p>
+            <div className="btns">
+              <button className="first-btn">LEARN MORE ABOUT US</button>
+              <button className="second-btn">SUBMIT REQUEST</button>
+            </div>
+
+          </div>
           <img height={900} src={FirstBigImg} alt="" />
         </div>
         <div className="car">
-        <h1>CREATEX</h1>
-        <h1>CONSTRUCTION</h1>
+          <div className="text">
+            <h1>CREATEX</h1>
+            <h1>CONSTRUCTION</h1>
+            <p>Cras ultrices leo vitae non viverra. Fringilla nisi quisque consequat, dignissim vitae proin ipsum sed. Pellentesque nec turpis purus eget pellentesque integer ipsum elementum felis. </p>
+            <div className="btns">
+              <button className="first-btn">LEARN MORE ABOUT US</button>
+              <button className="second-btn">SUBMIT REQUEST</button>
+            </div>
 
-          <img  height={900} src={FirstBigImg} alt="" />
-
-        </div>
-        <div className="car">
-        <h1>CREATEX</h1>
-        <h1>CONSTRUCTION</h1>
+          </div>
 
           <img height={900} src={FirstBigImg} alt="" />
 
         </div>
         <div className="car">
-        <h1>CREATEX</h1>
-        <h1>CONSTRUCTION</h1>
+          <div className="text">
+            <h1>CREATEX</h1>
+            <h1>CONSTRUCTION</h1>
+            <p>Cras ultrices leo vitae non viverra. Fringilla nisi quisque consequat, dignissim vitae proin ipsum sed. Pellentesque nec turpis purus eget pellentesque integer ipsum elementum felis. </p>
+            <div className="btns">
+              <button className="first-btn">LEARN MORE ABOUT US</button>
+              <button className="second-btn">SUBMIT REQUEST</button>
+            </div>
+
+          </div>
+          <img height={900} src={FirstBigImg} alt="" />
+
+        </div>
+        <div className="car">
+          <div className="text">
+            <h1>CREATEX</h1>
+            <h1>CONSTRUCTION</h1>
+            <p>Cras ultrices leo vitae non viverra. Fringilla nisi quisque consequat, dignissim vitae proin ipsum sed. Pellentesque nec turpis purus eget pellentesque integer ipsum elementum felis. </p>
+            <div className="btns">
+              <button className="first-btn">LEARN MORE ABOUT US</button>
+              <button className="second-btn">SUBMIT REQUEST</button>
+            </div>
+
+          </div>
 
           <img height={900} src={FirstBigImg} alt="" />
         </div>
       </Carousel>
-      </div>
+    </div>
 
-    </>
   );
 }
 
--- myapp/src/components/Navigation/Nav.jsx
+++ myapp/src/components/Navigation/Nav.jsx
@@ -23,5 +23,4 @@ function Nav() {
         </>
      );
 }
-
 export default Nav;
